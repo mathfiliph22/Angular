0f209d70d43a255cf968b60ff8d8446407a679bc
mathfiliph22 <matheusfiliph@hotmail.com>
2022-01-06 23:50:01 -0300

    componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContatosComponent } from './Views/contatos/contatos.component';
+import { HomeComponent } from './Views/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    
+    {path: "contatos" , component: ContatosComponent}
+    
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContatosComponent } from './Views/contatos/contatos.component';
+import { JogadorComponent } from './Views/jogador/jogador.component';
+import { HomeComponent } from './Views/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContatosComponent,
+    JogadorComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
